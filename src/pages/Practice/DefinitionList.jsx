24203c0b2c64d3ec093a0d3c0a7f74f1a045539d
ewsn0825 <ewsn0825@naver.com>
2023-08-11 11:01:19 +0900

    button 컴포넌트 분리

---
+++ src/pages/Practice/DefinitionList.jsx
@@ -0,0 +1,25 @@
+import ConditionalDisplay from './parts/ConditionalDisplay';
+import ConditionalRendering from './parts/ConditionalRendering';
+import DisplayingData from './parts/DisplayingData';
+import RenderingLists from './parts/RenderingLists';
+import { imageType, isShowReactImage, statusMessage } from './parts/data';
+
+function DefinitionList() {
+  const renderList = () => {
+    const renderListItem = (message) => <li key={message}>{message}</li>;
+    return statusMessage.map(renderListItem);
+  };
+
+  const allHidden = false;
+
+  return (
+    <dl className="descriptionList">
+      <DisplayingData hidden={allHidden} statusMessage={statusMessage} />
+      <ConditionalRendering hidden={allHidden} imageType={imageType} />
+      <ConditionalDisplay hidden={allHidden} isShowReactImage={isShowReactImage} />
+      <RenderingLists statusMessage={statusMessage} renderList={renderList} />
+    </dl>
+  );
+}
+
+export default DefinitionList;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     devSourcemap: true,
+  },
+  resolve: {
+    // alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+    // alias: [
+    //   { find: "@components", replacement: "/src/components" },
+    //   { find: "@", replacement: "/src" },
+    // ],
   },
 });
