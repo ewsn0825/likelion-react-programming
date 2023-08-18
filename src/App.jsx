908ace4b753401a6c9cd3e75b5672997f78bf082
ewsn0825 <ewsn0825@naver.com>
2023-08-18 15:24:19 +0900

    실습 전 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,83 @@
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider, NavLink } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
-import LearnStateAndEffects from './pages/LearnStateAndEffects';
-import ProductList from './pages/ProductList';
+
+const Nav = () => (
+  <nav>
+    <ul className="flex gap-4 p-5 font-extralight bg-slate-50/50 shadow-lg">
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/products"
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Products
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Home</h1>
+  </div>
+);
+const Products = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Products</h1>
+  </div>
+);
+const Contact = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Contact</h1>
+  </div>
+);
+
+// Router Object
+// 경로(path), 요소(element ← <Component />)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/products', element: <Products /> },
+      { path: '/contact', element: <Contact /> },
+    ],
+  },
+]);
+
+console.log(router);
 
 function App() {
-  const [isVisible] = useState(true);
-
   return (
     <div className="App">
-      <RootLayout>
-        <LearnStateAndEffects />
-        {isVisible && <ProductList />}
-      </RootLayout>
+      <RouterProvider router={router} />
     </div>
   );
 }
