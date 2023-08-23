ad9e16ac7e378d23d4f422aaaae43829a996fb4d
ewsn0825 <ewsn0825@naver.com>
2023-08-23 13:03:29 +0900

    리프팅 스테이드 다루기

--- src/learn/1-passing-props.jsx
+++ src/learn/1-passing-props.jsx
@@ -1,3 +1,4 @@
+import debounce from '@/utils/debounce';
 import { useState } from 'react';
 
 function PassingProps() {
@@ -29,11 +30,14 @@ export function Parent() {
 export function Child({ color, onChangeColor }) {
   return (
     <div className="Child flex items-center justify-center gap-8">
-      <p className='text-4xl font-extralight uppercase' style={{ color }}>Child</p>
+      <p className="text-4xl font-extralight uppercase" style={{ color }}>
+        Child
+      </p>
       <input
         type="color"
         aria-label="글자 색상 변경"
-        onChange={(e) => onChangeColor(e.target.value)}
+        defaultValue={color}
+        onChange={debounce((e) => onChangeColor(e.target.value), 500)}
       />
     </div>
   );
